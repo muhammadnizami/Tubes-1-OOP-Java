9dce0d35585a1ea95cedbb7604fd629f175a36ab
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-03-08 17:28:36 +0700

    Romawi - fix sedikit

--- src/token/Romawi.cpp
+++ src/token/Romawi.cpp
@@ -6,10 +6,6 @@ bool Romawi::canConvert(const std::string& s){ 	//menembalikan true bila s dapat
 		{"M","CM","D","CD","C","XC",
 		"L","XL","X","IX","V","IV","I",
 		"S","*"};
-	float u[] = 
-		{1000, 900, 500, 400, 100, 90,
-		50, 40, 10, 9, 5, 4, 1,
-		(float)1/(float)2, (float)1/(float)12};
 	int j = 0;
 	for (int i=0;i<15;i++)
 		while (!s.compare(j,rs[i].length(),rs[i])){
