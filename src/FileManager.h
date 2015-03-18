aec5858a3fde8f2ce82388592f7e0ac0960f5de0
muhammadnizami <muhammadnizami@users.noreply.github.com>
2015-03-18 14:47:20 +0700

    Update FileManager.h
    Doxygen

--- src/FileManager.h
+++ src/FileManager.h
@@ -2,6 +2,15 @@
 #define FileManager_h
 #include "history.h"
 #include <exception>
+
+/*!
+Responsibility
+kelas ini bertugas menyimpan histori ke berkas
+Hubungan dengan kelas lain
+kelas ini menggunakan kelas Histori pada salah satu metodanya
+Gambaran umum method
+kelas ini memiliki metoda Save
+*/
 class FileManager{
 public:
 	void Save(History H);
