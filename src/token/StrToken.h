09b231c0688ddcb1fa67dda3f79835ff9736c7c3
muhammadnizami <muhammadnizami@users.noreply.github.com>
2015-03-18 12:53:53 +0700

    Update StrToken.h
    Doxygennnnnn

--- src/token/StrToken.h
+++ src/token/StrToken.h
@@ -4,13 +4,20 @@
 #include <string>
 #include "Token.h"
 
-//kelas abstrak
+/*!
+Responsibility
+Kelas ini merupakan kelas abstrak. Tanggung jawabnya adalah mengkonversi satu jenis representasi token antara objek string dan token.
+Hubungan dengan kelas lain
+kelas ini diimplementasi oleh kelas Arab, Logika, Romawi, dan Operator.
+Gambaran umum method
+metodanya adalah mengkonversi dari string ke token dan mengkonversi dari token ke string, serta pengecekan apakah string tertentu dapat dikonversi olehnya
+*/
 class StrToken{
 	public:
 	virtual ~StrToken(){};
-	virtual bool canConvert(const std::string& s) = 0; 	//menembalikan true bila s dapat dikonversi ke token
-	virtual std::string toString (const Token& T) = 0;	//mengembalikan representasi string dari token T
-	virtual Token toToken(const std::string& s) = 0;	//mengembalikan representasi token dari string s
+	virtual bool canConvert(const std::string& s) = 0; 	//!mengembalikan true bila s dapat dikonversi ke token
+	virtual std::string toString (const Token& T) = 0;	//!mengembalikan representasi string dari token T
+	virtual Token toToken(const std::string& s) = 0;	//!mengembalikan representasi token dari string s
 };
 
 #endif
