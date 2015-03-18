93c8b5e4483684fd7bea456fccbc2b45e92e6fdd
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-03-18 08:50:58 +0700

    Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "Evaluator.h"
 #include "UI.h"
 
-//g++ main.cpp Evaluator.cpp token/converter.cpp token/Token.cpp token/Arab.cpp token/Logika.cpp token/Romawi.cpp token/mesintoken.cpp katakr/mesinkata.cpp katakr/mesinkar.cpp token/Operator.cpp 
+
 
 int main(){
 	Evaluator E;
