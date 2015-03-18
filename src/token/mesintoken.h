94d4e791fb6ec1334fa6aacdfff0fe0ee55a5d8f
muhammadnizami <muhammadnizami@users.noreply.github.com>
2015-03-18 18:49:01 +0700

    Doxygen - repair1

--- src/token/mesintoken.h
+++ src/token/mesintoken.h
@@ -6,23 +6,32 @@
 #include "converter.h"
 #include <string>
 
-//!tidak bisa untuk polymorphism
-//!virtual destruktor belum diimplementasi (tidak dibutuhkan untuk proyek ini)
-//!Responsibility
-//!Membaca ekspresi token per token
-//!Kelas ini menggunakan std::string. Kelas ini mewarisi Mesin Kata. Kelas ini mengandung kelas converter.
-//!Method kelas ini meliputi konstruktor yang menerima string pita karakter, START(), ADV(), GetCToken(). Start memulai mesin, ADV memajukan satu token, dan GetCToken() mengakses token yang sedang dibaca.
+//!tidak bisa untuk polymorphism\n
+//!virtual destruktor belum diimplementasi (tidak dibutuhkan untuk proyek ini)\n
+//!Responsibility\n
+//!Membaca ekspresi token per token\n
+//!Kelas ini menggunakan std::string. Kelas ini mewarisi Mesin Kata. Kelas ini mengandung kelas converter.\n
+//!Method kelas ini meliputi konstruktor yang menerima string pita karakter, START(), ADV(), GetCToken(). Start memulai mesin, ADV memajukan satu token, dan GetCToken() mengakses token yang sedang dibaca.\n
 class mesintoken:public mesinkata{
 public:
-	mesintoken (const std::string& ST); //! membentuk mesin token dengan pita karakter ST
-	mesintoken (const std::string& ST, const Converter& _cvt); //! membentuk mesin token dengan pita karakter ST dan converter cvt
+	//! membentuk mesin token dengan pita karakter ST
+	mesintoken (const std::string& ST); 
+	//! membentuk mesin token dengan pita karakter ST dan converter cvt
+	mesintoken (const std::string& ST, const Converter& _cvt); 
 
-	//Getter
-	Token GetCToken() const; //! prekondisi: !GetEnd()
+	//!Getter@{
+	//! prekondisi: !GetEnd()
+	Token GetCToken() const; 
+	//!@}
 
-	//override
-	void START(); //! prekondisi pita karakter tidak kosong
-	void ADV();//! prekondisi: !GetEnd()
+	//!override\n
+	//!method-method ini mengoverride dengan menambahkan konversi ke token dan disimpan di CToken
+	//!@{
+	//! prekondisi pita karakter tidak kosong
+	void START(); 
+	//! prekondisi: !GetEnd()
+	void ADV();
+	//!@}
 private:
 	Converter CVT;
 	Token CToken;
