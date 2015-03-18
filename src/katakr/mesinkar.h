65baf31067d094b2afb8d6add69e705754c3021b
deredet <lucky.cahyadi@yahoo.com>
2015-03-18 12:59:21 +0700

    Update mesinkar.h
    Doxygen

--- src/katakr/mesinkar.h
+++ src/katakr/mesinkar.h
@@ -4,28 +4,62 @@
 #include <string>
 
 class mesinkar{
+   /*! Kelas mesinkar berguna untuk membaca string input satu per stau
+   */
    public:
    mesinkar();
+   /*! konstruktor
+   */
    mesinkar (const std::string& _pitakar);
+   /*! konstruktor dengan paramater
+   */
    mesinkar (const mesinkar& m);
+   /*! copy constructor
+   */
    mesinkar& operator=(const mesinkar& m);
+   /*! operator assignment
+   */
    ~mesinkar();
-   //Getter
+   /*! destruktor
+   */
+   ///Getter
    char GetCC() const;
+   /*! mengembalikan character yang terakhir dibaca      
+   */
    bool GetEnd() const;
+   /*! mengembalikan apakah pembacaan string sudah selesai
+   */
    const std::string& GetPitaKarakter() const;
+   /*! mengembalikan string yang sedang dibaca
+   */
    int GetIdxChar() const;
-   //Setter
-   void SetCC(char CT);
-   void SetEnd(bool ET);
-   void SetPitaKarakter(const std::string& ST);
-   void SetIdxChar(int i);
-   //Fungsi
-   void START();
-   void ADV();
-   bool EOP() const;
+   /*! mengembalikan indeks karakter dalam string yang terakhir dibaca
+   */
+   ///Setter
+   void SetCC(char CT); 
+   /*!I.S : Sembarang
+   F.S : CC = CT*/
+   void SetEnd(bool ET); /*!
+   I.S : Sembarang
+   F.S : End = ET */
+   void SetPitaKarakter(const std::string& ST); 
+   /*!I.S : String tidak kosong
+   F.S PitaKarakter = ST*/
+   void SetIdxChar(int i); 
+   /*!I.S : Sembarang;
+   F.S : idxChar = i   */
+   ///Fungsi
+   void START(); 
+   /*! I.S : Sembarang
+   F.S L idxChar = 0, CC = char PitaKarakter pada idxChar, End = false*/
+   void ADV(); 
+   /*! I.S : START() sudah dipanggil
+   F.S : idxChar +=1, CC = char PitaKarakter pada idxChar, End = true jika EOP / false jika belum EOP
+   */
+   bool EOP() const; /*!
+   mengembalikan true jika End == true*/
    private:
-   char CC;
+   char CC; 
    bool End;
    std::string PitaKarakter;
    int idxChar;
