67a440b0bd1b344c0dbb8c30583f31a11f5320c2
muhammadnizami <muhammadnizami@users.noreply.github.com>
2015-03-18 18:24:55 +0700

    Doxygen - repair1

--- src/FileManager.h
+++ src/FileManager.h
@@ -4,19 +4,22 @@
 #include <exception>
 
 /*!
-Responsibility
-kelas ini bertugas menyimpan histori ke berkas
-Hubungan dengan kelas lain
-kelas ini menggunakan kelas Histori pada salah satu metodanya
-Gambaran umum method
-kelas ini memiliki metoda Save
+Responsibility\n
+kelas ini bertugas menyimpan histori ke berkas\n
+Hubungan dengan kelas lain\n
+kelas ini menggunakan kelas Histori pada salah satu metodanya\n
+Gambaran umum method\n
+kelas ini memiliki metoda Save\n
 */
 class FileManager{
 public:
+
+	//!menyimpan ke berkas dengan tambahan informasi tanggal dan jam
 	void Save(History H);
 		
 };
 
+//!exception yang dilempar oleh FileManager
 class FileManagerException: public std::exception{
 public:
 	virtual const char* what() const throw(){
