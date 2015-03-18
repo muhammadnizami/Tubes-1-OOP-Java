2becae770c6bd29e8f73a2e5ddfe74be7ae3dec6
muhammadnizami <muhammadnizami@users.noreply.github.com>
2015-03-18 18:31:29 +0700

    Doxygen - repair1

--- src/token/Operator.h
+++ src/token/Operator.h
@@ -7,25 +7,34 @@
 
 #define TIPETOKEN_COUNT 12 //harus diubah apabila Token.h berubah
 
-/*! kelas implementasi
+/*! kelas implementasi\n
 Kelas ini merupakan implementasi kelas StrToken dengan konversi sesuai aturan penulisan operator.
 */
 class Operator: public StrToken{
 	public:
 	Operator();
 	
-	bool StrRepComplies(const std::string& s); //!mengembalikan true bila s dapat digunakan sebagai representasi operator
-	void SetString(TipeToken _tkn, const std::string& s);
+	//!mengembalikan true bila s dapat digunakan sebagai representasi operator
+	bool StrRepComplies(const std::string& s); 
+	
+	
 		//!I.S.:	0<= (int) _tkn <TIPETOKEN_COUNT
 		//!	s tidak dapat dikonversi oleh Arab, Romawi, maupun Logika
+	void SetString(TipeToken _tkn, const std::string& s);
 		
-	std::string GetString(TipeToken _tkn);
+		
 		//!mengembalikan Opr[_tkn]
 		//!prekondisi: 0<=_tkn<TIPETOKEN_COUNT
+	std::string GetString(TipeToken _tkn);
 
-	virtual bool canConvert(const std::string& s); 	//!mengembalikan true bila s dapat dikonversi ke token
-	virtual std::string toString (const Token& T);	//!mengembalikan representasi string dari token T
-	virtual Token toToken(const std::string& s);	//!mengembalikan representasi token dari string s
+	//!mengembalikan true bila s dapat dikonversi ke token
+	virtual bool canConvert(const std::string& s); 	
+	
+	//!mengembalikan representasi string dari token T
+	virtual std::string toString (const Token& T);	
+	
+	//!mengembalikan representasi token dari string s
+	virtual Token toToken(const std::string& s);	
 
 	private:
 	std::string Opr[TIPETOKEN_COUNT];
