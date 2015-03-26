568a930401cae32907056cc848390e77728ec0af
muhammadnizami <muhammadnizami@users.noreply.github.com>
2015-03-26 10:34:53 +0700

    Update TipeToken.java

--- src/TipeToken.java
+++ src/TipeToken.java
@@ -1,3 +1,25 @@
 public enum TipeToken{
-
+  //karena identifikasi "and" sepertinya tidak dapat digunakan,
+  //maka seluruhnya diganti menjadi diawali huruf kapital
+  /*!
+  Tipe Token menyatakan apakah token merupakan suatu operator atau bilangan
+  */
+  Kali,
+  Bagi,
+  Tambah,
+  Kurang,
+  Div,
+  Mod,
+  Kurungbuka,
+  Kurungtutup,
+  And,
+  Or,
+  Not,
+  KurangDari,
+  KurangDariSamaDengan,
+  LebihDari,
+  LebihDariSamaDengan,
+  SamaDengan,
+  TidakSamaDengan,
+  Bilangan,
 }
