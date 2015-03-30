1bb6e6af8a50bcc479d336a6667732d21ac2bcbf
deredet <lucky.cahyadi@yahoo.com>
2015-03-30 16:02:41 +0700

    Update StrToken.java

--- src/StrToken.java
+++ src/StrToken.java
@@ -1,6 +1,6 @@
 public interface StrToken{
     //!mengembalikan true bila s dapat dikonversi ke token
-   bool canConvert(String s);
+   boolean canConvert(String s);
   //!mengembalikan representasi string dari token T
    String toString (Token T);
   //!mengembalikan representasi token dari string s
