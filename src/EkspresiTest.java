5797c33e5371a54783cc27e7e27f0fc0fd1c4669
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-05 10:05:05 +0700

    .

--- src/EkspresiTest.java
+++ src/EkspresiTest.java
@@ -15,5 +15,30 @@ public class EkspresiTest{
 		Ekspresi e = new Ekspresi("+ 4 3");
 		assert(e.getVal().equals("7"));
 	}
+	@Test
+	public void TestPrefix2(){
+		Ekspresi e = new Ekspresi("not and < 4 3 true");
+		assert(e.getVal().equals("true"));
+	}
+	@Test
+	public void TestPrefix3(){
+		Ekspresi e = new Ekspresi(">= 3 3");
+		assert(e.getVal().equals("true"));
+	}
+	@Test
+	public void TestPrefix4(){
+		Ekspresi e = new Ekspresi("div 4 3");
+		assert(e.getVal().equals("1"));
+	}
+	@Test
+	public void TestPrefix5(){
+		Ekspresi e = new Ekspresi("not and < 4 3 true");
+		assert(e.getVal().equals("true"));
+	}
+	@Test
+	public void TestPrefix6(){
+		Ekspresi e = new Ekspresi("* -3 9");
+		assert(e.getVal().equals("-27"));
+	}
 }
 
