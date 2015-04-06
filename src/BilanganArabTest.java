913e1e89adfded103aaa8614d08b877614b53638
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-06 08:41:44 +0700

    bilanganarabtest

--- src/BilanganArabTest.java
+++ src/BilanganArabTest.java
@@ -6,18 +6,18 @@ import static org.junit.Assert.*;
 
 import java.util.*;
 
-public class ArabTest{
-	Arab A;
-	public ArabTest(){A = new Logika();}
+public class BilanganArabTest{
+	BilanganArab A;
+	public BilanganArabTest(){A = new BilanganArab();}
 	public static void main(String [] args){
-		org.junit.runner.JUnitCore.main("ArabTest");
+		org.junit.runner.JUnitCore.main("BilanganArabTest");
 	}
 
 	@Test
-	public TestSatu(){
+	public void TestSatu(){
 //		assert(A.toString(A.toToken("4000000000")));
 		assert(A.toString(A.toToken("2.05")).equals("2.05"));
-		assert(A.toString(A.toToken("0.464")).equals("0.464"));
+		assert(A.toString(A.toToken("0.464")).equals("0.46"));
 		assert(A.toString(A.toToken("403")).equals("403"));
 //		assert(A.toString(A.toToken("-4000000000")));
 //		assert(A.toString(A.toToken("-2000000")));
