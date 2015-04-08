0717582d65ecd085befaaff739a9231d8cb32df0
deredet <lucky.cahyadi@yahoo.com>
2015-04-08 18:26:09 +0700

    Update BilanganLogika.java
    buat JDoc

--- src/BilanganLogika.java
+++ src/BilanganLogika.java
@@ -1,12 +1,17 @@
-
-
+/** Responsibility
+ * Kelas ini merupakan implementasi kelas StrToken dengan konversi sesuai aturan bilangan Logika
+ * Hubungan dengan kelas lain
+ * mengimplementasi StrToken
+ * Gambaran umum method
+ * metodanya adalah implementasi metoda StrToken
+*/
 public class BilanganLogika implements StrToken{
-	//!mengembalikan true bila s dapat dikonversi ke token
+	/**mengembalikan true bila s dapat dikonversi ke token*/
 	public boolean canConvert(/*const*/ String s){ 	
 		return s.equals("true") || s.equals("false");
 	}
 
-	//!mengembalikan representasi string dari token T
+	/**mengembalikan representasi string dari token T*/
 	public String toString (/*const*/ Token T){
 		assert(T.getTkn() == TipeToken.Bilangan);
 		assert(T.getTipeBilangan() == Tipe._bool);
@@ -14,7 +19,7 @@ public class BilanganLogika implements StrToken{
 			return "true";
 		else	return "false";
 	}
-	//!mengembalikan representasi token dari string s
+	/**mengembalikan representasi token dari string s*/
 	public Token toToken(/*const*/ String s){	
 		assert(canConvert(s));
 		Token retval = new Token();
@@ -23,6 +28,7 @@ public class BilanganLogika implements StrToken{
 		else	retval.SetBilangan(true);
 		return retval;
 	}
+	/**mengembalikan ekspresi reguler dari kelas turunan StrToken tersebut*/
 	public String getRegEx(){
 		return "(true)|(false)";
 	}
