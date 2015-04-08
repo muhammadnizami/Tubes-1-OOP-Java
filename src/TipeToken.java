03fa8655d0458fe8c19ae36e23d9ff981b763988
deredet <lucky.cahyadi@yahoo.com>
2015-04-08 19:02:00 +0700

    Update TipeToken.java
    update Jdoc

--- src/TipeToken.java
+++ src/TipeToken.java
@@ -1,7 +1,7 @@
 public enum TipeToken{
   //karena identifikasi "and" sepertinya tidak dapat digunakan,
   //maka seluruhnya diganti menjadi diawali huruf kapital
-  /*!
+  /**
   Tipe Token menyatakan apakah token merupakan suatu operator atau bilangan
   */
   Kali,
