2a960548c812415c959ab277e1094e895e02fa41
deredet <lucky.cahyadi@yahoo.com>
2015-04-08 19:19:02 +0700

    Update StrToken.java
    update JDoc

--- src/StrToken.java
+++ src/StrToken.java
@@ -1,10 +1,11 @@
+
 public interface StrToken{
-	//!mengembalikan true bila s dapat dikonversi ke token
+	/**mengembalikan true bila s dapat dikonversi ke token*/
 	public boolean canConvert(String s);
-	//!mengembalikan representasi string dari token T
+	/**mengembalikan representasi string dari token T*/
 	public String toString (Token T);
-	//!mengembalikan representasi token dari string s
+	/**mengembalikan representasi token dari string s*/
 	public Token toToken(String s); 
-	//!mengembalikan ekspresi reguler dari kelas turunan StrToken tersebut
+	/**mengembalikan ekspresi reguler dari kelas turunan StrToken tersebut*/
 	public String getRegEx();
 }
